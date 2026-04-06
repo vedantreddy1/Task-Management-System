92cf227441bba490742b45db776b2294a94a3200
VedantR27 <vedant@lifecircle.in>
2026-04-06 18:34:03 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,24 +1,23 @@
 // server.js
 
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 // const helmet = require("helmet");
-const cors = require("cors")
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-mongoose
-  .connect("mongodb://localhost:27017/taskManagement")
-  .then(() => console.log("DB Connected"));
+// .connect("mongodb://localhost:27017/taskManagement")
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("DB Connected"));
 // app.use(helmet());
-
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/task", require("./routes/taskRoute"));
 
 
+const PORT = process.env.PORT||5000 ;
 
-app.listen(5000, () => console.log("Server running"));
+app.listen(PORT, () => console.log("Server running",PORT));
